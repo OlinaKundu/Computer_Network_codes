bb5e3444716882fa89e5053c072b7dbf0586dd5e
Olina Kundu <olinakundu55@gmail.com>
2026-04-26 19:40:46 +0530

    Refactor client1.c by removing client setup
    Removed client initialization code.

--- asg4/client1.c
+++ asg4/client1.c
@@ -18,10 +18,7 @@ bzero((char *)&server, sizeof(server));
 server.sin_family = AF_INET;
 server.sin_port = htons(SERVER_PORT);
 server.sin_addr.s_addr = inet_addr(SERVER_IP);
-bzero((char *)&client, sizeof(client));
-client.sin_family = AF_INET;
-client.sin_port = htons(CLIENT_PORT);
-client.sin_addr.s_addr = inet_addr(CLIENT_IP);
+  
 sd = socket(AF_INET, SOCK_STREAM, 0);
 connect(sd, (struct sockaddr *)&server, sizeof(server));
 do
