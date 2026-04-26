9733688070a7a54be1c3fe68981ed453b732439b
Olina Kundu <olinakundu55@gmail.com>
2026-04-26 20:32:41 +0530

    Refactor server.c to streamline main function

--- asg10/server.c
+++ asg10/server.c
@@ -1,47 +1,24 @@
+int main(){
+        struct sockaddr_in client, server;
+        int sd, nsd, n, count = 0, clen = sizeof(client);
+        char msg[512], msg1[512], ack[] = "acknowledgement recieved";
 
-    #include <sys/types.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
+        bzero((char *)&server, sizeof(server));
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = inet_addr(SERVER_IP);
+        server.sin_port = htons(SERVER_PORT);
 
-#define SERVER_IP "127.0.0.1"
-#define SERVER_PORT 6555
+        sd = socket(AF_INET, SOCK_STREAM, 0);
+        bind(sd, (struct sockaddr *)&server, sizeof(server));
+        listen(sd, 5);
 
-int main() {
-    struct sockaddr_in client, server;
-    int sd, nsd, clen = sizeof(client);
-    int next_expected = 0; // This will be our ACK
-    char frame;
-
-    bzero((char *)&server, sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = inet_addr(SERVER_IP);
-    server.sin_port = htons(SERVER_PORT);
-
-    sd = socket(AF_INET, SOCK_STREAM, 0);
-    bind(sd, (struct sockaddr*)&server, sizeof(server));
-    listen(sd, 5);
-
-    printf("Server listening...\n");
-    nsd = accept(sd, (struct sockaddr*)&client, &clen);
-
-    while (1) {
-        // Use nsd, not sd
-        int res = recv(nsd, &frame, sizeof(char), 0);
-        if (res <= 0) break; // Client disconnected
-
-        printf("Received frame: %c\n", frame);
-        
-        // Logic: Send the index of the NEXT frame we want
-        next_expected++; 
-        send(nsd, &next_expected, sizeof(int), 0);
-        printf("Sent ACK: %d\n", next_expected);
-    }
-
-    close(nsd);
-    close(sd);
-    return 0;
+        while(1){
+                nsd = accept(sd, (struct sockaddr *)&client, &clen);
+                do{
+                        memset(msg, 0x0, 512);
+                        recv(nsd, msg, 512, 0);
+                        printf("recieved message: %s\n", msg);
+                        send(nsd, ack, strlen(ack)+1, 0);
+                }while(strcmp(msg, "stop"));
+        }
 }
